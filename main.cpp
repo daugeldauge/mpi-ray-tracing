2e0d464d5fe0c8f9202ff85f1c1375b867150b70
Artem Daugel-Dauge <daugeldauge@gmail.com>
2014-12-29 00:10:00 +0300

    Implement estimated main function

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <string>
+
+#include "mpi.h"
+
+#include "scene.h"
+#include "tracer.h"
+
+
+
+int 
+main(int argc, const char *argv[])
+{
+  MPI::Init(argc, argv);
+
+  int width = 640, height = 480;
+  std::string path = "result.png";
+  
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i]; 
+    if (arg.compare("-w")) {
+      width = std::stoi(argv[++i]);
+    } else if (arg.compare("-h")) {
+      height = std::stoi(argv[++i]);
+    } else if (arg.compare("-f")) {
+      path = argv[++i];
+    }
+  }
+
+  Scene scene;
+
+  Tracer tracer = {scene, width, height};
+  tracer.renderImage();
+  tracer.saveImage();
+
+  MPI::Finalize();
+  return 0;
+}
---
+++ tracer.h
@@ -0,0 +1,5 @@
+#ifndef TRACER_H
+#define TRACER_H
+
+
+#endif
